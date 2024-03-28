246e5cd613884d7324b78b477252f716ca0e6a89
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-28 14:43:02 +0100

    Add  new modify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../src/Components/Navbar";
+import Banner from "./Components/Banner";
+import AboutMe from "./Components/AboutMe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Banner />
+        <AboutMe />
+        <Routes></Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
