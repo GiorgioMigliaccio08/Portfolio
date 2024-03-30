4581ab09bf8beeebe47162bcd50a7ba2d43422a0
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-30 16:49:32 +0100

    Modify AbuotMe

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,23 +1,74 @@
 import React from "react";
-import MyFoto from "../Asset/MyFoto.jpg";
+import Foto from "../Asset/Profilo.jpg";
+
+import { FaLaptopCode } from "react-icons/fa";
+import { HiPaintBrush } from "react-icons/hi2";
 
 function AboutMe() {
   return (
-    <section className="about-me" id="about">
-      <div className="about-img">
-        <img src={MyFoto} alt="Foto Profilo"></img>
+    <section id="about">
+      <div className="about-me">
+        <div className="about-img">
+          <img src={Foto} alt="Foto Profilo"></img>
+          <span className="circle-spin"></span>
+        </div>
+
+        <div class="about-content">
+          <h2 class="heading">
+            About <span>Me</span>
+          </h2>
+          <h3>Hi, I'm Here To Help Your Next Project! </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br />
+            odio incidunt! Ipsam hic molestias ad voluptas doloribus ratione
+            animi alias dicta quo.
+          </p>
+        </div>
       </div>
-      <div class="about-content">
+
+      <div className="servizi">
         <h2 class="heading">
-          About <span>Me</span>
+          My<span>Servives</span>
         </h2>
-        <h3>Hi, I'm Here To Help Your Next Project! </h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          <br />
-          odio incidunt! Ipsam hic molestias ad voluptas doloribus ratione animi
-          alias dicta quo.
-        </p>
+        <div className="services-container">
+          <div className="services-box">
+            <FaLaptopCode className="icon-servizi" />
+            <h3>Front-End Developer</h3>
+            <p>
+              Al contrario di quanto si pensi, Lorem Ipsum non è semplicemente
+              una sequenza casuale AC semplicemente una sequenza casuale
+            </p>
+            <a href="#" className="btn-servizi">
+              {" "}
+              Read More
+            </a>
+          </div>
+          <div className="services-box">
+            <FaLaptopCode className="icon-servizi" />
+            <h3>Back-End Developer</h3>
+            <p>
+              Al contrario di quanto si pensi, Lorem Ipsum non è semplicemente
+              una sequenza casuale AC semplicemente una sequenza casuale
+            </p>
+            <a href="#" className="btn-servizi">
+              {" "}
+              Read More
+            </a>
+          </div>
+          <div className="services-box">
+            <HiPaintBrush className="icon-servizi" />
+            <h3>Grafic Designer</h3>
+            <p>
+              Al contrario di quanto si pensi, Lorem Ipsum non è semplicemente
+              una sequenza casuale AC semplicemente una sequenza casuale
+            </p>
+            <a href="#" className="btn-servizi">
+              {" "}
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
