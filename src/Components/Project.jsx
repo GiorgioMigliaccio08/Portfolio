883727cb316e1695f17dc3b0590180a130dcaa77
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-04-03 17:44:16 +0200

    Modify Project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProgettoUno from "../Asset/Heart Archive Hub.jpg";
-import { FaLink } from "react-icons/fa";
+import { FaLink, FaRegFileCode } from "react-icons/fa";
+import { FiExternalLink } from "react-icons/fi";
 
 function Project() {
   return (
@@ -14,9 +15,14 @@ function Project() {
           <div className="progetti-layer">
             <h4>Heart Archive Hub</h4>
             <p>Al contrario di quanto si pensi,</p>
-            <a href="#">
-              <FaLink className="icona-progetti" />
-            </a>
+            <div>
+              <a href="#">
+                <FaRegFileCode className="icona-progetti" />
+              </a>
+              <a href="#">
+                <FiExternalLink className="icona-progetti" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="progetti-box">
@@ -24,9 +30,14 @@ function Project() {
           <div className="progetti-layer">
             <h4>Heart Archive Hub</h4>
             <p>Al contrario di quanto si pensi,</p>
-            <a href="#">
-              <FaLink className="icona-progetti" />
-            </a>
+            <div>
+              <a href="#">
+                <FaRegFileCode className="icona-progetti" />
+              </a>
+              <a href="#">
+                <FiExternalLink className="icona-progetti" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="progetti-box">
@@ -34,9 +45,14 @@ function Project() {
           <div className="progetti-layer">
             <h4>Heart Archive Hub</h4>
             <p>Al contrario di quanto si pensi,</p>
-            <a href="#">
-              <FaLink className="icona-progetti" />
-            </a>
+            <div>
+              <a href="#">
+                <FaRegFileCode className="icona-progetti" />
+              </a>
+              <a href="#">
+                <FiExternalLink className="icona-progetti" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="progetti-box">
@@ -44,29 +60,14 @@ function Project() {
           <div className="progetti-layer">
             <h4>Heart Archive Hub</h4>
             <p>Al contrario di quanto si pensi,</p>
-            <a href="#">
-              <FaLink className="icona-progetti" />
-            </a>
-          </div>
-        </div>
-        <div className="progetti-box">
-          <img src={ProgettoUno} alt="Foto Progetto"></img>
-          <div className="progetti-layer">
-            <h4>Heart Archive Hub</h4>
-            <p>Al contrario di quanto si pensi,</p>
-            <a href="#">
-              <FaLink className="icona-progetti" />
-            </a>
-          </div>
-        </div>
-        <div className="progetti-box">
-          <img src={ProgettoUno} alt="Foto Progetto"></img>
-          <div className="progetti-layer">
-            <h4>Heart Archive Hub</h4>
-            <p>Al contrario di quanto si pensi,</p>
-            <a href="#">
-              <FaLink className="icona-progetti" />
-            </a>
+            <div>
+              <a href="#">
+                <FaRegFileCode className="icona-progetti" />
+              </a>
+              <a href="#">
+                <FiExternalLink className="icona-progetti" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
