78ad2166cae10c7b18c3b08db64a314c029c526c
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-04-08 14:36:34 +0200

    Add Contact.jsx

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section id="contact">
+      <div>
+        <p>In Lavorazione!!</p>
+      </div>
+    </section>
+  );
+}
+export default Contact;
