9c7ae16d84f236d314af34e832422ea30723841d
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-04-09 15:57:05 +0200

    Add modify to Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,10 +2,30 @@ import React from "react";
 
 function Contact() {
   return (
-    <section id="contact">
-      <div>
-        <p>In Lavorazione!!</p>
-      </div>
+    <section className="contact" id="contact">
+      <h2 class="heading">
+        Contact <span>Me</span>
+      </h2>
+      <form action="#">
+        <div className="input-box">
+          <input type="text" placeholder="Nome Completo" />
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="input-box">
+          <input type="number" placeholder="Cellulare" />
+          <input type="text" placeholder="Descrizione" />
+        </div>
+        <textarea
+          name=""
+          id=""
+          cols={20}
+          rows={10}
+          placeholder="Il tuo messaggio"
+        ></textarea>
+        <imput type="submit" value="Invia" className="btn">
+          Invia!
+        </imput>
+      </form>
     </section>
   );
 }
