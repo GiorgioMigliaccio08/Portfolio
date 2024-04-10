5cba0bc6882ca5606cbe8dd1d5836adb09b58d65
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-04-10 23:54:43 +0200

    Add new modify to Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,32 +1,61 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userId = "TUO_USER_ID";
+
+    emailjs.sendForm("SERVICE_ID", "TEMPLATE_ID", e.target, userId).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+
+    e.target.reset();
+  };
+
   return (
     <section className="contact" id="contact">
-      <h2 class="heading">
+      <h2 className="heading">
         Contact <span>Me</span>
       </h2>
-      <form action="#">
+      <form onSubmit={handleSubmit}>
         <div className="input-box">
-          <input type="text" placeholder="Nome Completo" />
-          <input type="email" placeholder="Email" />
+          <input
+            type="text"
+            name="fullName"
+            placeholder="Nome Completo"
+            required
+          />
+          <input type="email" name="email" placeholder="Email" required />
         </div>
         <div className="input-box">
-          <input type="number" placeholder="Cellulare" />
-          <input type="text" placeholder="Descrizione" />
+          <input type="tel" name="phone" placeholder="Cellulare" required />
+          <input
+            type="text"
+            name="description"
+            placeholder="Descrizione"
+            required
+          />
         </div>
         <textarea
-          name=""
-          id=""
+          name="message"
           cols={20}
           rows={10}
           placeholder="Il tuo messaggio"
+          required
         ></textarea>
+        <button type="submit" className="btn-contact">
+          Invia
+        </button>
       </form>
-      <imput type="submit" value="Invia" className="btn-contact">
-        Invia!
-      </imput>
     </section>
   );
 }
+
 export default Contact;
