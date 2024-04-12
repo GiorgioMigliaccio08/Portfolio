58d221fd604441e0fdaba7d6949b52a4ada50796
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-04-12 17:55:04 +0200

    Add modify to Project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import ProgettoUno from "../Asset/Heart Archive Hub.jpg";
+import ProgettoUno from "../Asset/Heart-Archive-Hub.jpg";
+import ProgettoDue from "../Asset/Mercedes-Project.jpg";
+import ProgettoTre from "../Asset/Netflix.png";
+import ProgettoQuattro from "../Asset/Medium.jpg";
 import { FaLink, FaRegFileCode } from "react-icons/fa";
 import { FiExternalLink } from "react-icons/fi";
 
@@ -26,12 +29,12 @@ function Project() {
           </div>
         </div>
         <div className="progetti-box">
-          <img src={ProgettoUno} alt="Foto Progetto"></img>
+          <img src={ProgettoDue} alt="Foto Progetto"></img>
           <div className="progetti-layer">
-            <h4>Heart Archive Hub</h4>
+            <h4>Mercedes Store</h4>
             <p>Al contrario di quanto si pensi,</p>
             <div>
-              <a href="#">
+              <a href="https://github.com/GiorgioMigliaccio08/Mercedes-Store">
                 <FaRegFileCode className="icona-progetti" />
               </a>
               <a href="#">
@@ -41,12 +44,12 @@ function Project() {
           </div>
         </div>
         <div className="progetti-box">
-          <img src={ProgettoUno} alt="Foto Progetto"></img>
+          <img src={ProgettoTre} alt="Foto Progetto"></img>
           <div className="progetti-layer">
-            <h4>Heart Archive Hub</h4>
+            <h4>Netflix Clone</h4>
             <p>Al contrario di quanto si pensi,</p>
             <div>
-              <a href="#">
+              <a href="https://github.com/GiorgioMigliaccio08/U3-W2-L3-FS0423IT-Navigazione-per-Neftlix-React">
                 <FaRegFileCode className="icona-progetti" />
               </a>
               <a href="#">
@@ -56,12 +59,12 @@ function Project() {
           </div>
         </div>
         <div className="progetti-box">
-          <img src={ProgettoUno} alt="Foto Progetto"></img>
+          <img src={ProgettoQuattro} alt="Foto Progetto"></img>
           <div className="progetti-layer">
-            <h4>Heart Archive Hub</h4>
+            <h4>Medium Clone</h4>
             <p>Al contrario di quanto si pensi,</p>
             <div>
-              <a href="#">
+              <a href="https://github.com/GiorgioMigliaccio08/U2-W1-L5-FS0423IT-Weekly-Project-Medium-Page">
                 <FaRegFileCode className="icona-progetti" />
               </a>
               <a href="#">
